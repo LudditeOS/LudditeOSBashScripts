d35510cc75896592aa4f25bf220f2ab2659bfca5
Oliver Staub <hitch.dynasty_0m@icloud.com>
2023-12-10 15:05:53 +0100

    dockerfile angepasst

--- sync/syncSource.sh
+++ sync/syncSource.sh
@@ -1,21 +1,14 @@
 #!/bin/bash -v
 
 
-cd /home/app
-mkdir LudditeOS
-cd LudditeOS
-mkdir bin
-mkdir source
-
-
 #Repo installieren
-curl https://storage.googleapis.com/git-repo-downloads/repo > $HOME/gitroot/LudditeOS/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
 chmod +x /home/app/LudditeOS/bin/repo
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/app/LudditeOS/bin:$PATH
 
-cd source
+cd /home/app/LudditeOS/source
 repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
 repo sync
 
