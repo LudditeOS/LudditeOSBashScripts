534867183e2d66d22c9d0f7cbb82b477193009a0
Oliver Staub <hitch.dynasty_0m@icloud.com>
2023-12-13 19:17:00 +0100

    changed sync to oliver folder

--- sync/syncSource.sh
+++ sync/syncSource.sh
@@ -2,12 +2,12 @@
 
 
 #Repo installieren
-curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > /home/oliver/LudditeOS/bin/repo
 chmod +x /home/app/LudditeOS/bin/repo
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
-PATH=/home/app/LudditeOS/bin:$PATH
+PATH=/home/oliver/LudditeOS/bin:$PATH
 
-cd /home/app/LudditeOS/source
+cd /home/oliver/LudditeOS/source
 repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
 repo sync
