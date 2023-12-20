0a25fd8ddeb7af5da6edccc6534495d21b041530
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-20 20:42:27 +0100

    pfad angepasst

--- emulator/emulatewithoutcontainer.sh
+++ emulator/emulatewithoutcontainer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -v
 
+
+#Repo installieren
+curl https://storage.googleapis.com/git-repo-downloads/repo > /home/oliver/LudditeOS/bin/repo
 chmod +x /home/oliver/LudditeOS/bin/repo
 
+#Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/oliver/LudditeOS/bin:$PATH
 
 cd /home/oliver/LudditeOS/source
 repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
-
-
 
 . build/envsetup.sh
 lunch aosp_x86_64-eng
