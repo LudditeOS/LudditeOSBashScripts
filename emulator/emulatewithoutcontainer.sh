cad072550e5bfac9d81ec3fe7e6bde3e27fe98b4
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-22 17:51:52 +0100

    delete repo first

--- emulator/emulatewithoutcontainer.sh
+++ emulator/emulatewithoutcontainer.sh
@@ -2,18 +2,19 @@
 
 
 #Repo installieren
-sudo curl https://storage.googleapis.com/git-repo-downloads/repo > /home/oliver/LudditeOS/bin/repo
+sudo rm -rf /home/oliver/LudditeOS/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > /home/oliver/LudditeOS/bin/repo
 chmod +x /home/oliver/LudditeOS/bin/repo
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/oliver/LudditeOS/bin:$PATH
 
 cd /home/oliver/LudditeOS/source
-sudo repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
+repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
 
 . build/envsetup.sh
 lunch aosp_x86_64-eng
-emulator --foo
+emulator @foo
 
 
 
