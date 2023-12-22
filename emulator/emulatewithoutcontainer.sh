225b12ee62317e5a0b2f8fcaeb9962b4691235b8
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-23 00:55:40 +0100

    repo init mit sudo

--- emulator/emulatewithoutcontainer.sh
+++ emulator/emulatewithoutcontainer.sh
@@ -16,7 +16,7 @@ chmod +x /home/oliver/LudditeOS/bin/repo
 PATH=/home/oliver/LudditeOS/bin:$PATH
 
 cd /home/oliver/LudditeOS/source
-repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
+sudo repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
 
 . build/envsetup.sh
 lunch aosp_x86_64-eng
