d8e19279ee33a3239305d0beb7cdfb3422e712f1
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2023-12-30 23:41:32 +0100

    remove sudo

--- build/createnewcontainer.sh
+++ build/createnewcontainer.sh
@@ -1,4 +1,4 @@
-sudo docker rm LudditeBuild
-sudo docker rmi luddite-build-img:1.0
+docker rm LudditeBuild
+docker rmi luddite-build-img:1.0
 git pull
-sudo docker build -t luddite-build-img:1.0 .
+docker build -t luddite-build-img:1.0 .
