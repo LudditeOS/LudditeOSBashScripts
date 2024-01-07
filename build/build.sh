130866032e175bde4963a7be0c6c6f964dd9f321
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-07 18:31:46 +0100

    switched to lineageOS

--- build/build.sh
+++ build/build.sh
@@ -4,17 +4,23 @@ echo "build script start"
 
 #Repo installieren
 curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
-chmod +x /home/app/LudditeOS/bin/repo
+chmod a+x /home/app/LudditeOS/bin/repo
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/app/LudditeOS/bin:$PATH
 
-cd /home/app/LudditeOS/source
+cd /home/app/LudditeOS/lineage
 repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r78
+git lfs install
+git config --global trailer.changeid.key "Change-Id"
+
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+
 
 #start build
-. build/envsetup.sh
-lunch sdk_phone_x86_64-userdebug
-m
+source . build/envsetup.sh
+lunch lineage_sdk_phone_x86_64-eng
+mka
 cd /home/app/LudditeOS 
-chmod -R 777 source
+chmod -R 777 lineage
