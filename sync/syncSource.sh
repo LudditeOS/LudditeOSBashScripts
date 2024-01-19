5ee751475aeed65acd4ed35d48be41d36a5296b4
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-19 17:53:26 +0100

    test without muppets

--- sync/syncSource.sh
+++ sync/syncSource.sh
@@ -4,7 +4,6 @@ mkdir -p /home/app/LudditeOS/android/lineage
 #Repo installieren
 curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
 chmod a+x /home/app/LudditeOS/bin/repo
-cp roomservice.xml /home/app/LudditeOS/bin/repo/local_manifests
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/app/LudditeOS/bin:$PATH
@@ -19,8 +18,8 @@ export CCACHE_EXEC=/usr/bin/ccache
 
 cd /home/app/LudditeOS/android/lineage
 repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-sleep 1000
-#repo sync
+
+repo sync
 
 #start build
 #source build/envsetup.sh
