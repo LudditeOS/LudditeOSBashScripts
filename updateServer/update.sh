894e6f6e06337aa08fc296ac47dcd4a5cb204692
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-21 20:49:58 +0100

    create local manifests directory

--- updateServer/update.sh
+++ updateServer/update.sh
@@ -1,4 +1,4 @@
-sudo apt update        # Fetches the list of available updates
-sudo apt upgrade       # Installs some updates; does not remove packages
-sudo apt full-upgrade  # Installs updates; may also remove some packages, if needed
-sudo apt autoremove    # Removes any old packages that are no longer needed
+sudo apt update -y        # Fetches the list of available updates
+sudo apt upgrade -y       # Installs some updates; does not remove packages
+sudo apt full-upgrade -y  # Installs updates; may also remove some packages, if needed
+sudo apt autoremove -y    # Removes any old packages that are no longer needed
