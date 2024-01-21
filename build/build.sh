48fa76edf40c50af1717446fb90d08ea3fb1dd20
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-21 21:51:41 +0100

    outcomment build.sh

--- build/build.sh
+++ build/build.sh
@@ -22,11 +22,11 @@ export CCACHE_EXEC=/usr/bin/ccache
 source build/envsetup.sh
 breakfast star2lte
 
-croot
-brunch star2lte
+# croot
+# brunch star2lte
 
-cd /home/app/LudditeOS 
-chmod -R 777 android
+# cd /home/app/LudditeOS 
+# chmod -R 777 android
 
 
 
