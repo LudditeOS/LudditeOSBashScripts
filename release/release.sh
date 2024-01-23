e818b4bb00c11cb2e4c66914a0e10ca9f88dc03d
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-23 20:21:11 +0100

    added sudo

--- release/release.sh
+++ release/release.sh
@@ -1,5 +1,5 @@
 timestamp=$(date +%Y%m%d%H%M%S)
 cd /home/oliver/LudditeOS/release
-mkdir "$timestamp"
+sudo mkdir "$timestamp"
 cp /home/oliver/LudditeOS/android/lineage/out/target/product/star2lte/*.zip /home/oliver/LudditeOS/release/"$timestamp"
 
