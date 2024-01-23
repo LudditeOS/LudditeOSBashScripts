e9a4a6c6d94adf446e8b027f95bfdcf126638091
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-23 10:56:16 +0100

    testcommit

--- release/release.sh
+++ release/release.sh
@@ -1,3 +1,3 @@
-#zweite ssd mounten und freigeben, und dann als Release benützen
+#zweite ssd mounten und freigeben, und dann als Release benützendd
 
 cp lineage-20.0-20240122-UNOFFICIAL-star2lte.zip relase/irgendwas
