e0f011d047272e56adc215d0891271715d119444
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-23 20:39:00 +0100

    adjust path

--- release/release.sh
+++ release/release.sh
@@ -2,5 +2,5 @@ timestamp=$(date +%Y-%m-%d-%H-%M-%S)
 cd /home/oliver/LudditeOS/release
 sudo mkdir "$timestamp"
 sudo cp /home/oliver/LudditeOS/android/lineage/out/target/product/star2lte/*.zip /home/oliver/LudditeOS/release/"$timestamp"
-cd /home/oliver/gitroot/
+cd /home/oliver/gitroot/release
 
