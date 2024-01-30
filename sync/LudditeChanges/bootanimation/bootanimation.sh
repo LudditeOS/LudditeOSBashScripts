b1911d5fece5a1de5366d4d5271435ba87eae97c
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-01-30 22:39:48 +0100

    Adding bootanimation

---
+++ sync/LudditeChanges/bootanimation/bootanimation.sh
@@ -0,0 +1,8 @@
+#copy .tar File
+#change Android.mk file
+
+#führen wir das skript auch von einem docker container aus aus ?
+
+
+
+cp bootanimation.tar /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/bootanimation.tar
