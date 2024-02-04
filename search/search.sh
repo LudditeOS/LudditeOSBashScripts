59c1d20b1b3f16430ca4f52d30f0aba6b6ce6b17
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-04 15:38:42 +0100

    sudo

--- search/search.sh
+++ search/search.sh
@@ -1,6 +1,6 @@
 #in das LudditeRoot Verzeichnis kopieren
 
-find /home/oliver/LudditeOS/ -iname '*'$1'*' > results.txt
+sudo find /home/oliver/LudditeOS/ -iname '*'$1'*' > results.txt
 
 
-egrep -ir '*'$1'*' /home/oliver/LudditeOS >> results.txt
+sudo egrep -ir '*'$1'*' /home/oliver/LudditeOS >> results.txt
