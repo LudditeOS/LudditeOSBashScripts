423b5d4f8e021d998391a19c4ee66e0f447cbf6b
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-11 15:03:16 +0100

    fix filename

--- search/search.sh
+++ search/search.sh
@@ -1,6 +1,6 @@
 #in das LudditeRoot Verzeichnis kopieren
 
-sudo find /home/oliver/LudditeOS/ -iname '*'$1'*' > $1+results.txt
+sudo find /home/oliver/LudditeOS/ -iname '*'$1'*' > ${1}results.txt
 
 
-sudo egrep  -I -n -H -ir '*'$1'*' --exclude-dir=out /home/oliver/LudditeOS >> $1+results.txt
+sudo egrep  -I -n -H -ir '*'$1'*' --exclude-dir=out /home/oliver/LudditeOS >> ${1}results.txt
