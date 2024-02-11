0d4ea8f973a3473593afa56ef68db42af2788957
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-11 15:01:28 +0100

    exclude binaries

--- search/search.sh
+++ search/search.sh
@@ -3,4 +3,4 @@
 sudo find /home/oliver/LudditeOS/ -iname '*'$1'*' > $1+results.txt
 
 
-sudo egrep -ir '*'$1'*' --exclude-dir=out /home/oliver/LudditeOS >> $1+results.txt
+sudo egrep  -I -n -H -ir '*'$1'*' --exclude-dir=out /home/oliver/LudditeOS >> $1+results.txt
