487bcebc2bf84d7dbc51de79bde191affdfeaca0
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-11 15:20:15 +0100

    exclude java files

--- search/search.sh
+++ search/search.sh
@@ -3,4 +3,4 @@
 sudo find /home/oliver/LudditeOS/ -iname '*'$1'*' > ${1}results.txt
 
 
-sudo egrep  -I -n -H -ir '*'$1'*' --exclude-dir=out /home/oliver/LudditeOS >> ${1}results.txt
+sudo egrep -I -n -H -ir --exclude-dir=out --exclude='*.java' --exclude='*.html' --exclude='*/test/*' '*'$1'*' /home/oliver/LudditeOS >> ${1}results.txt
