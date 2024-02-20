8227e711029ff03db64ecc722dfbe44763f0131e
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-20 19:45:17 +0100

    adjustments to cp command

--- sync/LudditeChanges/focuslauncher/focuslauncher.sh
+++ sync/LudditeChanges/focuslauncher/focuslauncher.sh
@@ -6,8 +6,7 @@ buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product/"
 
 mkdir -p "$appdirectory"
 wget "$url" -P "$appdirectory"
-unzip "$appdirectory/V-0.1-Snapshot.zip" -d "$appdirectory"
-cp "$(dirname "$0")/Android.mk" "$appdirectory"
+cp -f "$(dirname "$0")/Android.mk" "$appdirectory"
 
 mkdir -p "$buildDirectory"
 patch "$appdirectory/handheld_product.mk" < "$(dirname "$0")/FocusLauncherPatch"
