d8548920c504a23dac307e7894ec9af42818c4a8
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-20 22:30:58 +0100

    fix typo

--- sync/LudditeChanges/focuslauncher/focuslauncher.sh
+++ sync/LudditeChanges/focuslauncher/focuslauncher.sh
@@ -1,15 +1,15 @@
 #install Focuslauncher
 
 url="https://github.com/ostaubzug/FocusLauncherRelease/releases/download/V0.1/app-release.apk"
-appdirectory="/home/oliver/LudditeOS/android/lineage/packages/apps/FocusLauncher/"
-buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product/"
+appdirectory="/home/oliver/LudditeOS/android/lineage/packages/apps/FocusLauncher"
+buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product"
 
 mkdir -p "$appdirectory"
 wget "$url" -P "$appdirectory"
 cp Android.mk "$appdirectory"
 
 mkdir -p "$buildDirectory"
-patch "$appdirectory/handheld_product.mk" < FocusLauncherPatch
+patch "$buildDirectory/handheld_product.mk" < FocusLauncherPatch
 
 
 
