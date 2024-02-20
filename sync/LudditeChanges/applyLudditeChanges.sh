718babb9a810c16094fd8ae68b792a3a96927505
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-20 22:28:14 +0100

    changes

--- sync/LudditeChanges/applyLudditeChanges.sh
+++ sync/LudditeChanges/applyLudditeChanges.sh
@@ -1,12 +1,7 @@
 #bootanimation.sh
-source bootanimation/bootanimation.sh
-source focuslauncher/focuslauncher.sh
+cd /bootanimation/
+sh bootanimation/bootanimation.sh
 
+cd /focuslauncher/
+sh focuslauncher/focuslauncher.sh
 
-
-
-
-
-## wo lege ich die apk dateien ab? Ich bräuchte ein binär Repository für dependencys ...
-## Auch nur schon damit ich mehrere Versionen builden kann
-## Github pipeline mit lokalem worker um apk's gleich zu deployen
--- sync/LudditeChanges/focuslauncher/focuslauncher.sh
+++ sync/LudditeChanges/focuslauncher/focuslauncher.sh
@@ -1,15 +1,15 @@
-url="https://github.com/ostaubzug/FocusLauncherRelease/releases/download/V0.1/app-release.apk"
+#install Focuslauncher
 
+url="https://github.com/ostaubzug/FocusLauncherRelease/releases/download/V0.1/app-release.apk"
 appdirectory="/home/oliver/LudditeOS/android/lineage/packages/apps/FocusLauncher/"
 buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product/"
 
-
 mkdir -p "$appdirectory"
 wget "$url" -P "$appdirectory"
-cp -f "$(basename "$0")"/Android.mk "$appdirectory"
+cp Android.mk "$appdirectory"
 
 mkdir -p "$buildDirectory"
-patch "$appdirectory/handheld_product.mk" < "$(dirname "$0")"/FocusLauncherPatch
+patch "$appdirectory/handheld_product.mk" < FocusLauncherPatch
 
 
 
