550c6f891bfd67244232381d4dfe9ecb27ffc7d5
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-20 19:41:12 +0100

    adjusting applyChanges

--- sync/LudditeChanges/bootanimation/bootanimation.sh
+++ sync/LudditeChanges/bootanimation/bootanimation.sh
@@ -1,2 +1,2 @@
-cp bootanimation.tar /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/bootanimation.tar
-patch /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/Android.mk < MkPatch
+cp "$(dirname "$0")/bootanimation.tar" /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/bootanimation.tar
+patch /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/Android.mk < "$(dirname "$0")/MkPatch"
--- sync/LudditeChanges/focuslauncher/focuslauncher.sh
+++ sync/LudditeChanges/focuslauncher/focuslauncher.sh
@@ -1,19 +1,16 @@
-url="https://github.com/ostaubzug/focuslauncher/archive/refs/tags/V-0.1-Snapshot.zip"
+url="https://github.com/ostaubzug/FocusLauncherRelease/releases/download/V0.1/app-release.apk"
 
-directory="/home/oliver/LudditeOS/android/lineage/packages/apps/FocusLauncher/"
+appdirectory="/home/oliver/LudditeOS/android/lineage/packages/apps/FocusLauncher/"
 buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product/"
 
 
-mkdir -p "$directory"
-
-wget "$url" -P "$directory"
-
-unzip "$directory/V-0.1-Snapshot.zip" -d "$directory"
-
-cp Android.mk "$directory"
+mkdir -p "$appdirectory"
+wget "$url" -P "$appdirectory"
+unzip "$appdirectory/V-0.1-Snapshot.zip" -d "$appdirectory"
+cp "$(dirname "$0")/Android.mk" "$appdirectory"
 
 mkdir -p "$buildDirectory"
-patch "$directory/handheld_product.mk" < FocusLauncherPatch
+patch "$appdirectory/handheld_product.mk" < "$(dirname "$0")/FocusLauncherPatch"
 
 
 
