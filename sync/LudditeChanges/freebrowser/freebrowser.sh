bb7cfe590242e3fbd692c822bcd905e47e1fbe7c
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-03-01 11:07:35 +0100

    added FreeBrowser

---
+++ sync/LudditeChanges/freebrowser/freebrowser.sh
@@ -0,0 +1,15 @@
+#install Focuslauncher
+
+url="https://f-droid.org/repo/org.woheller69.browser_2600.apk"
+appdirectory="/home/oliver/LudditeOS/android/lineage/packages/apps/FreeBrowser"
+buildDirectory="/home/oliver/LudditeOS/android/lineage/build/target/product"
+
+mkdir -p "$appdirectory"
+wget -O "$appdirectory/FreeBrowser.apk" "$url"
+cp Android.mk "$appdirectory"
+
+mkdir -p "$buildDirectory"
+patch "$buildDirectory/handheld_product.mk" < FocusLauncherPatch
+
+
+
