81d458ac9b71fc7d90154da274f9374e5147667f
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-10-25 15:04:52 +0200

    added new files

---
+++ build/LudditeChanges/bootanimation/bootanimation.sh
@@ -0,0 +1,2 @@
+cp bootanimation.tar /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/bootanimation.tar
+patch /home/oliver/LudditeOS/android/lineage/vendor/lineage/bootanimation/Android.mk < MkPatch
