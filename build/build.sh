5cdb6907cd7217866394b7936020ce8856a34329
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-10-27 10:53:34 +0100

    change repo logic

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash -v
-mkdir -p /home/app/LudditeOS/android/lineage
 
-#Repo installieren
-curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
-chmod a+x /home/app/LudditeOS/bin/repo
+# Define the target directory and repo binary path
+TARGET_DIR="/home/app/LudditeOS/android/lineage"
+REPO_PATH="/home/app/LudditeOS/bin/repo"
+
+# Check if the directory is missing or empty, and if repo is not installed
+if [ ! -d "$TARGET_DIR" ] || [ -z "$(ls -A "$TARGET_DIR")" ] || [ ! -f "$REPO_PATH" ]; then
+
+  mkdir -p "$TARGET_DIR"
+
+  curl https://storage.googleapis.com/git-repo-downloads/repo > "$REPO_PATH"
+  chmod a+x "$REPO_PATH"
+  echo "Repo installed successfully."
+else
+  echo "Repo already installed, skipping installation."
+fi
+
+
+
+
 
 #Eigenes Path Skript schreiben das den Pfad in einer neuen Terminal Session wieder setzen kann
 PATH=/home/app/LudditeOS/bin:$PATH
@@ -18,7 +33,7 @@ export CCACHE_EXEC=/usr/bin/ccache
 if [[ "${STAR2LTE,,}" == "true" ]]; then
     echo "Luddite Build star2lte lineage 20"
     cd /home/app/LudditeOS/android/lineage
-    repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+    repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs --force-sync
     mkdir -p /home/app/LudditeOS/android/lineage/.repo/local_manifests
     cp /home/app/config/roomservice-star2lte.xml /home/app/LudditeOS/android/lineage/.repo/local_manifests/roomservice.xml
 
@@ -36,7 +51,7 @@ fi
 if [[ "${ORIOLE,,}" == "true" ]]; then
     echo "Luddite Build oriole lineage 21"
     cd /home/app/LudditeOS/android/lineage
-    repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+    repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --force-sync
     mkdir -p /home/app/LudditeOS/android/lineage/.repo/local_manifests
     cp /home/app/config/roomservice-oriole.xml /home/app/LudditeOS/android/lineage/.repo/local_manifests/roomservice.xml
 
