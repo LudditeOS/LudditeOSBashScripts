bac5a0c41e8d6b04f06be62e012061b2911ae478
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-02 16:50:16 +0100

    change the repo folder

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -v
 
-mkdir -p /home/app/LudditeOS/android/lineage
-mkdir -p /home/app/dockerbin/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/dockerbin/bin/repo
 
-chmod a+x /home/app/dockerbin/bin/repo
-PATH=/home/app/dockerbin/bin:$PATH
-git lfs install
-git config --global trailer.changeid.key "Change-Id"
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
 
 
 
@@ -18,7 +9,20 @@ if [[ "${STAR2LTE,,}" == "true" ]]; then
     echo "Luddite Build star2lte lineage 20"
     
     rm -rf /home/app/LudditeOS/android/lineage
+    rm -rf /home/app/LudditeOS/bin
+    
+
     mkdir -p /home/app/LudditeOS/android/lineage
+    mkdir -p /home/app/LudditeOS/bin
+    curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
+    chmod a+x /home/app/LudditeOS/bin/repo
+
+    PATH=/home/app/LudditeOS/bin:$PATH
+    git lfs install
+    git config --global trailer.changeid.key "Change-Id"
+    export USE_CCACHE=1
+    export CCACHE_EXEC=/usr/bin/ccache
+
 
     cd /home/app/LudditeOS/android/lineage
     repo init -u /home/app/LudditeOS/LineageMirror/LineageOS/android.git -b lineage-20.0 --git-lfs
@@ -27,6 +31,7 @@ if [[ "${STAR2LTE,,}" == "true" ]]; then
     cp /home/app/config/roomservice-star2lte.xml /home/app/LudditeOS/android/lineage/.repo/local_manifests/roomservice.xml
 
     repo sync --force-remove-dirty --force-sync
+    
     chmod -R a+x /home/app/LudditeChanges
     . /home/app/LudditeChanges/applyLudditeChanges.sh
 
