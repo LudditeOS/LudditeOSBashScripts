93fe849b1a3db01b3f4fda497b4b4fd79901301e
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-16 10:21:06 +0100

    fix added optional Libraries to Ncphotos & Protonpass

--- build/LudditeChanges/standardApps/ncphotos/Android.mk
+++ build/LudditeChanges/standardApps/ncphotos/Android.mk
@@ -6,5 +6,6 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := NcPhotos.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar org.apache.http.legacy
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
