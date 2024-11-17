aec5091d9a33cf5f728db3b09828833f3844fd00
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-17 10:22:10 +0100

    whatsapp remove preopt

--- build/LudditeChanges/standardApps/whatsapp/Android.mk
+++ build/LudditeChanges/standardApps/whatsapp/Android.mk
@@ -7,5 +7,7 @@ LOCAL_SRC_FILES := Whatsapp.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_ENFORCE_USES_LIBRARIES := false
 LOCAL_OPTIONAL_USES_LIBRARIES := com.sec.android.app.multiwindow org.apache.http.legacy androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
