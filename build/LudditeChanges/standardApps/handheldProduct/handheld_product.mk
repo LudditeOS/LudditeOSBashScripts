77a69e91445d76390ba45e90f5282aeb59edcb7d
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-21 20:58:44 +0100

    fix handheld_product

--- build/LudditeChanges/standardApps/handheldProduct/handheld_product.mk
+++ build/LudditeChanges/standardApps/handheldProduct/handheld_product.mk
@@ -24,12 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 # /product packages
 PRODUCT_PACKAGES += \
     FocusLauncher \
-    Aegis \
-    Davx \
-    NcPhotos \
-    Phoenix \
-    ProtonPass \
-    Whatsapp \
     Calendar \
     Contacts \
     DeskClock \
