7080da629e26c56b7785a00fd585d3492d43f71d
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-22 14:33:25 +0100

    change to PRODUCT_VENDOR_PROPERTIES build.prop property, possible deprecation

--- build/build.sh
+++ build/build.sh
@@ -29,10 +29,8 @@ repo sync
 chmod -R a+x /home/app/LudditeChanges
 . /home/app/LudditeChanges/applyLudditeChanges.sh
 
-sed -i "1s;^;PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://192.168.1.195/api/v1/$BUILD_TARGET/unofficial/{}\n\n;" /home/app/LudditeOS/android/lineage/vendor/lineage/config/common.mk
 
-source build/envsetup.sh
-breakfast ${BUILD_TARGET}
+sed -i "1s;^;PRODUCT_VENDOR_PROPERTIES += lineage.updater.uri=https://192.168.1.195/api/v1/$BUILD_TARGET/unofficial/{}\n\n;" /home/app/LudditeOS/android/lineage/vendor/lineage/config/common.mk
 
 source build/envsetup.sh
 breakfast ${BUILD_TARGET}
