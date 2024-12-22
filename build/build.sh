f0b8bab070bf26534bfd0add2b0330060f00ddee
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-12-22 22:08:00 +0100

    fix configuration

--- build/build.sh
+++ build/build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -v
 
-
-rm -rf /home/app/LudditeOS/bin
-rm -rf /home/app/LudditeOS/android
-    
-mkdir -p /home/app/LudditeOS/android/lineage
-mkdir -p /home/app/LudditeOS/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
-chmod a+x /home/app/LudditeOS/bin/repo
-
-PATH=/home/app/LudditeOS/bin:$PATH
-git lfs install
-git config --global trailer.changeid.key "Change-Id"
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-
-
-cd /home/app/LudditeOS/android/lineage
-
 if [ "$FULL_BUILD" = true ]; then
+    rm -rf /home/app/LudditeOS/bin
+    rm -rf /home/app/LudditeOS/android
+    
+    mkdir -p /home/app/LudditeOS/android/lineage
+    mkdir -p /home/app/LudditeOS/bin
+    curl https://storage.googleapis.com/git-repo-downloads/repo > /home/app/LudditeOS/bin/repo
+    chmod a+x /home/app/LudditeOS/bin/repo
+
+    PATH=/home/app/LudditeOS/bin:$PATH
+    git lfs install
+    git config --global trailer.changeid.key "Change-Id"
+    export USE_CCACHE=1
+    export CCACHE_EXEC=/usr/bin/ccache
+
+
+    cd /home/app/LudditeOS/android/lineage
+
+
     repo init -u http://www.github.com/LineageOS/android -b lineage-${BUILD_LINEAGE_VERSION} --git-lfs --reference=/home/app/LudditeOS/LineageMirror/LineageOS/android.git
     repo sync
     
@@ -30,6 +30,11 @@ if [ "$FULL_BUILD" = true ]; then
     chmod -R a+x /home/app/LudditeChanges
     . /home/app/LudditeChanges/applyLudditeChanges.sh
 fi
+
+cd /home/app/LudditeOS/android/lineage
+PATH=/home/app/LudditeOS/bin:$PATH
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
 
 source build/envsetup.sh
 breakfast ${BUILD_TARGET}
