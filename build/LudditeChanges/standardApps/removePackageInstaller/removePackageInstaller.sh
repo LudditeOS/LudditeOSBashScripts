1b5c9c08c9fa995e80daf744da015bab4081cc73
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-12-24 09:19:43 +0100

    remove PackageInstaller Install Button

--- build/LudditeChanges/standardApps/removePackageInstaller/removePackageInstaller.sh
+++ build/LudditeChanges/standardApps/removePackageInstaller/removePackageInstaller.sh
@@ -1,1 +1,1 @@
-patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/AndroidManifest.xml < /home/app/LudditeChanges/standardApps/removePackageInstaller/changes.patch
+patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java < /home/app/LudditeChanges/standardApps/removePackageInstaller/changes.patch
