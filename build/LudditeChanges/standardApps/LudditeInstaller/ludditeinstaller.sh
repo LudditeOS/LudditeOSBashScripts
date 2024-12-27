c9476b96608c6569161c5fcff006743b1455bcd6
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-12-27 14:24:53 +0100

    rename

---
+++ build/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
@@ -0,0 +1,9 @@
+url="https://github.com/ostaubzug/LudditeInstaller/releases/download/V0.3/ludditeinstaller.apk"
+appdirectory="/home/app/LudditeOS/android/lineage/packages/apps/LudditeInstaller"
+
+if [ -d "$appdirectory" ]; then
+    rm -rf "$appdirectory"
+fi
+mkdir -p "$appdirectory"
+wget -O "$appdirectory/ludditeinstaller.apk" "$url"
+cp /home/app/LudditeChanges/standardApps/LudditeInstaller/Android.mk "$appdirectory"
