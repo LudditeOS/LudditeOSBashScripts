17ea24a5ec6bccad376b930bc112c4a2764434d1
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-12-27 14:23:42 +0100

    rename

--- build/LudditeChanges/standardApps/PackageInstaller/packageinstaller.sh
+++ build/LudditeChanges/standardApps/PackageInstaller/packageinstaller.sh
@@ -1,4 +1,3 @@
 patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java < /home/app/LudditeChanges/standardApps/PackageInstaller/activity.patch
 patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/src/com/android/packageinstaller/InstallStart.java < /home/app/LudditeChanges/standardApps/PackageInstaller/installstart.patch
 patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/AndroidManifest.xml < /home/app/LudditeChanges/standardApps/PackageInstaller/manifest.patch
-
