b6cfed6575dc4779314990ba0c49c19084236720
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-01 12:27:17 +0100

    installer V0.44

--- build/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
+++ build/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
@@ -1,4 +1,4 @@
-url="https://github.com/ostaubzug/LudditeInstaller/releases/download/V0.4.1/ludditeinstaller.apk"
+url="https://github.com/ostaubzug/LudditeInstaller/releases/download/V0.4.4/ludditeinstaller.apk"
 appdirectory="/home/app/LudditeOS/android/lineage/packages/apps/LudditeInstaller"
 
 if [ -d "$appdirectory" ]; then
