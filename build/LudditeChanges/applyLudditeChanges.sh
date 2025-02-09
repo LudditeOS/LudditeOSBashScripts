8db788cb6c7aa63b42dd3fa2f759b664a5f00d75
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-09 09:51:52 +0100

    Add packageinstaller 22.1 scripts

--- build/LudditeChanges/applyLudditeChanges.sh
+++ build/LudditeChanges/applyLudditeChanges.sh
@@ -1,5 +1,13 @@
-bash /home/app/LudditeChanges/bootanimation/bootanimation.sh
+#!/bin/bash
+set -e 
 
+bash /home/app/LudditeChanges/bootanimation/bootanimation.sh
 bash /home/app/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
+bash /home/app/LudditeChanges/standardApps/removeBrowser/removeBrowser.sh
+
+
+if [[ "${BUILD_LINEAGE_VERSION}" > "22.1" ]] || [[ "${BUILD_LINEAGE_VERSION}" = "22.1" ]]; then
 bash /home/app/LudditeChanges/standardApps/PackageInstaller/packageinstaller.sh
-#bash /home/app/LudditeChanges/standardApps/removeBrowser/removeBrowser.sh
+else
+bash /home/app/LudditeChanges/standardApps/PackageInstaller221/packageinstaller.sh
+fi
