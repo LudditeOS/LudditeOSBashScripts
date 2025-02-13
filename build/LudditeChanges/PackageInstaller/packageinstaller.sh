72a1212c27dfcb58d920b8f961dfdd911cd44e90
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-13 21:27:03 +0100

    add untracked files

---
+++ build/LudditeChanges/PackageInstaller/packageinstaller.sh
@@ -0,0 +1,3 @@
+patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/src/com/android/packageinstaller/PackageInstallerActivity.java < /home/app/LudditeChanges/PackageInstaller/activity.patch
+patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/src/com/android/packageinstaller/InstallStart.java < /home/app/LudditeChanges/PackageInstaller/installstart.patch
+patch /home/app/LudditeOS/android/lineage/frameworks/base/packages/PackageInstaller/AndroidManifest.xml < /home/app/LudditeChanges/PackageInstaller/manifest.patch
---
+++ build/LudditeChanges/removeBrowser22/removeBrowser22.sh
@@ -0,0 +1,2 @@
+patch /home/app/LudditeOS/android/lineage/device/generic/common/gsi_product.mk < /home/app/LudditeChanges/removeBrowser22/gsi_product.patch
+patch /home/app/LudditeOS/android/lineage/build/make/target/product/handheld_product.mk < /home/app/LudditeChanges/removeBrowser22/handheld.patch
