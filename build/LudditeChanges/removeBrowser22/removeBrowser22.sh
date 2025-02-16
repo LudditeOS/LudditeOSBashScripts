ace1f3d93cc55242e79b14f48873f00dd1c88d4a
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-16 10:53:20 +0100

    added common jelly patch

--- build/LudditeChanges/removeBrowser22/removeBrowser22.sh
+++ build/LudditeChanges/removeBrowser22/removeBrowser22.sh
@@ -1,2 +1,3 @@
 patch /home/app/LudditeOS/android/lineage/device/generic/common/gsi_product.mk < /home/app/LudditeChanges/removeBrowser22/gsi_product.patch
 patch /home/app/LudditeOS/android/lineage/build/make/target/product/handheld_product.mk < /home/app/LudditeChanges/removeBrowser22/handheld.patch
+patch /home/app/LudditeOS/android/lineage/vendor/lineage/config/common.mk < /home/app/LudditeChanges/removeBrowser22/common.patch
