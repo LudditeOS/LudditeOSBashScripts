108a6d19fd463cb74e065df3e7831a2e8a7ea580
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-18 18:14:12 +0100

    edit removebrowser20

--- build/LudditeChanges/removeBrowser20/removeBrowser20.sh
+++ build/LudditeChanges/removeBrowser20/removeBrowser20.sh
@@ -1,3 +1,2 @@
 patch /home/app/LudditeOS/android/lineage/device/generic/common/gsi_product.mk < /home/app/LudditeChanges/removeBrowser20/gsi_product.patch
 patch /home/app/LudditeOS/android/lineage/build/make/target/product/handheld_product.mk < /home/app/LudditeChanges/removeBrowser20/handheld.patch
-patch /home/app/LudditeOS/android/lineage/vendor/lineage/config/common.mk < /home/app/LudditeChanges/removeBrowser20/common.patch
