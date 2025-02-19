81bc95c6201bb9578c9ae3f5898ebf2fda9ef20c
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-19 19:00:10 +0100

    improve applyLudditeChanges.sh

--- build/LudditeChanges/applyLudditeChanges.sh
+++ build/LudditeChanges/applyLudditeChanges.sh
@@ -9,7 +9,8 @@ bash /home/app/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
 if [ "${BUILD_LINEAGE_VERSION}" = "22.1" ]; then
 bash /home/app/LudditeChanges/PackageInstaller221/packageinstaller.sh
 bash /home/app/LudditeChanges/removeBrowser22/removeBrowser22.sh
-else
+fi
+if [ "${BUILD_LINEAGE_VERSION}" = "20.0" ]; then
 bash /home/app/LudditeChanges/PackageInstaller/packageinstaller.sh
 bash /home/app/LudditeChanges/removeBrowser20/removeBrowser20.sh
 fi
