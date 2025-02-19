4808d20f3f4b66827f51cc69025f659be96e879c
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-02-19 18:58:20 +0100

    fix remove browser version 20.0

--- build/LudditeChanges/applyLudditeChanges.sh
+++ build/LudditeChanges/applyLudditeChanges.sh
@@ -6,7 +6,7 @@ bash /home/app/LudditeChanges/standardApps/LudditeInstaller/ludditeinstaller.sh
 
 
 
-if [[ "${BUILD_LINEAGE_VERSION}" > "22.1" ]] || [[ "${BUILD_LINEAGE_VERSION}" = "22.1" ]]; then
+if [ "${BUILD_LINEAGE_VERSION}" = "22.1" ]; then
 bash /home/app/LudditeChanges/PackageInstaller221/packageinstaller.sh
 bash /home/app/LudditeChanges/removeBrowser22/removeBrowser22.sh
 else
--- build/LudditeChanges/removeBrowser20/removeBrowser20.sh
+++ build/LudditeChanges/removeBrowser20/removeBrowser20.sh
@@ -1,2 +1,3 @@
 patch /home/app/LudditeOS/android/lineage/device/generic/common/gsi_product.mk < /home/app/LudditeChanges/removeBrowser20/gsi_product.patch
 patch /home/app/LudditeOS/android/lineage/build/make/target/product/handheld_product.mk < /home/app/LudditeChanges/removeBrowser20/handheld.patch
+patch /home/app/LudditeOS/android/lineage/vendor/lineage/config/common_mobile.mk < /home/app/LudditeChanges/removeBrowser20/common_mobile.patch
